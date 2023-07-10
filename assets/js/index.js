1bcb1857aedd0ad1d1fd60a4a1efa1b896bb750b
lean-dro <72211521+lean-dro@users.noreply.github.com>
2023-07-10 15:56:34 -0300

    Estilização + correção na geração de blocos aleatórios + inserção do quicksort

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
 
 
 var qtdBloco = 8
-
+var posicaoInicial = 0;
+var posicaoFinal = 0
 var blocos = [
     {tipo:"terra",valor:-1},
     {tipo:"terra2",valor:1},
@@ -27,6 +28,7 @@ blocos[5],
 blocos[6],
 blocos[7],
 ]
+var ordem = ordemCorreta;
 
 var divCamadas1 = document.getElementById("camadas_1");
 var divCamadas2 = document.getElementById("camadas_2");
@@ -55,49 +57,36 @@ function iniciarMundo() {
 
 
 var camadasAleatorias = []
+
+
+
+function reiniciarCamadaAleatoria() {
+  window.location.reload()
+}
+
 function misturarCamadas() {
     if(camadasAleatorias.length < 11){
-        console.log(camadasAleatorias.length)
-        var posicaoMax = ordemCorreta.length-1
-        var posicaoMin = 0
-    
-        var intervalo = (posicaoMax-posicaoMin)+1
-    
-        var posicaoAletatoria = parseInt(Math.random() * intervalo)
+            var posicaoMax = ordem.length-1
+            var posicaoMin = 0
+        
+            var intervalo = (posicaoMax-posicaoMin)+1
+            
+            var posicaoAletatoria = parseInt(Math.random() * intervalo)
 
-        console.log(posicaoAletatoria)
-       
-        var camadaValida = true
-        var contagemTerra2 = 0
-        if(camadasAleatorias.length == 0){
-            camadasAleatorias.push(ordemCorreta[posicaoAletatoria])
+            if(ordem[posicaoAletatoria] != null){
+                camadasAleatorias.push(ordem[posicaoAletatoria]);
+                ordem[posicaoAletatoria] = null  
+            }
+           
             return misturarCamadas()
         }else{
-            for(var i = 0; i<camadasAleatorias.length; i++){
-                if(camadasAleatorias[i].valor == ordemCorreta[posicaoAletatoria].valor){
-                    if(camadasAleatorias[i].valor == 1){
-                        contagemTerra2++
-                        if(contagemTerra2 > 4){
-                            camadaValida = false
-                        }
-                    }else{
-                        camadaValida = false
-                        
-                    }
-                  
-                }
-            }
-            if(camadaValida){
-                camadasAleatorias.push(ordemCorreta[posicaoAletatoria])
-            }
-            return misturarCamadas()
+            posicaoFinal = camadasAleatorias.length-1;
+            return listarCamadasAleatorias()
         }
-    }else{
-        listarCamadasAleatorias()
-    }
 }
 
 function listarCamadasAleatorias() {
+    divCamadasAleatorias.innerHTML = "";
     for(var camadas = 0; camadas < camadasAleatorias.length; camadas++){
         var linha = document.createElement("div")
         linha.classList.add("linha")
@@ -111,6 +100,44 @@ function listarCamadasAleatorias() {
             divCamadasAleatorias.appendChild(linha)
         }
     }
+}
+
+
+
+var bloco = ""
+function quicksort(vetor, comeco, fim) {
+    if(comeco < fim){
+            setTimeout(() => {
+            var posicaoPivot = particionar(vetor, comeco, fim)
+
+            quicksort(vetor, comeco, (posicaoPivot-1))
+            quicksort(vetor, (posicaoPivot+1), fim)
+
+        }, 2000);
+    }
+}
+
+function particionar(vetor, comeco, fim) {
+    var pivot = vetor[comeco].valor
+    var i = comeco
+
+    for(var j = comeco +1; j<=fim; j++){
+        var valorBlocoComparacao = vetor[j].valor
+        if(valorBlocoComparacao <= pivot){
+            i+=1
+            var camada_guardada = vetor[i]
+            vetor[i] = vetor[j]
+            vetor[j] = camada_guardada
+        }
+    }
+    
+    camada_guardada = vetor[comeco]
+     vetor[comeco] = vetor[i]
+    vetor[i] = camada_guardada
+  
+   
+    listarCamadasAleatorias()
+    return i   
 }
 
 
